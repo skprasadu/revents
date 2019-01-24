d8da65c348f01373481b90e6ee37ac2fe1425c7c
Krishna Prasad <skprasadu@yahoo.com>
2019-01-23 21:52:53 -0800

    implemented settingsdashboard

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
 import EventDetailedPage from "../../features/events/EventDetailed/EventDetailedPage";
 import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
@@ -14,16 +14,27 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
+        <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/events" component={EventDashboard} />
-          <Route path="/event/:id" component={EventDetailedPage} />
-          <Route path="/people" component={PeopleDashboard} />
-          <Route path="/profile/:id" component={UserDetailedPage} />
-          <Route path="/settings" component={SettingsDashboard} />
-          <Route path="/createEvent" component={EventForm} />
-        </Container>
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import SettingsNav from "./SettingsNav";
+import AboutPage from './AboutPage'
+import BasicPage from './BasicPage'
+import AccountPage from './AccountPage'
+import PhotoPage from './PhotoPage'
 
 function SettingsDashboard() {
   return (
-    <div>
-      <h1>Settings Dashboard</h1>
-    </div>
-  )
+    <Grid>
+      <Grid.Column width={12}>
+        <Switch>
+          <Redirect exact from='/settings' to='/settings/basics' />
+          <Route path="/settings/basics" component={BasicPage} />
+          <Route path="/settings/about" component={AboutPage} />
+          <Route path="/settings/photos" component={PhotoPage} />
+          <Route path="/settings/account" component={AccountPage} />
+        </Switch>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <SettingsNav />
+      </Grid.Column>
+    </Grid>
+  );
 }
 
-export default SettingsDashboard
+export default SettingsDashboard;
