08d1fa5c015d2036a07fa6282cd5512b76b41a0f
Krishna Prasad <skprasadu@yahoo.com>
2019-01-24 21:32:56 -0800

    Implemented testReducers

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -7,6 +7,7 @@ import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard
 import UserDetailedPage from "../../features/user/UserDetailedPage/UserDetailedPage";
 import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 import EventForm from "../../features/events/EventForm/EventForm";
+import TestComponent from "../../features/testarea/TestComponent";
 import HomePage from "../../features/home/HomePage";
 import NavBar from "../../features/nav/NavBar/NavBar";
 
@@ -25,6 +26,7 @@ class App extends Component {
               <Container className="main">
                 <Switch>
                   <Route path="/events" component={EventDashboard} />
+                  <Route path="/test" component={TestComponent} />
                   <Route path="/event/:id" component={EventDetailedPage} />
                   <Route path="/people" component={PeopleDashboard} />
                   <Route path="/profile/:id" component={UserDetailedPage} />
