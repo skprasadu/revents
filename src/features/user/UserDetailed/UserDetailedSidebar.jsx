e53b61cc10ef16327f3e85fb48f808ed76df4927
Krishna Prasad <skprasadu@yahoo.com>
2019-02-04 19:04:04 -0800

    Completed Section 18

---
+++ src/features/user/UserDetailed/UserDetailedSidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid, Segment, Button } from "semantic-ui-react";
+
+const UserDetailedSidebar = ({ profile }) => {
+  return (
+    <Grid.Column width={4}>
+      <Segment>
+        <Button color="teal" fluid basic content="Edit Profile" />
+      </Segment>
+    </Grid.Column>
+  );
+};
+
+export default UserDetailedSidebar;
